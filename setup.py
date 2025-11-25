0dce723b9b480219c912d74ecd5bc3f0199d0f5c
marhiru <marcosgamern@gmail.com>
2025-11-25 13:26:37 -0300

    feat: created setup class

---
+++ setup.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+
+class Setup:
+    targets: list[str] = []
+
+    def __init__(self, capture: str):
+        self = capture
+        Setup.targets.append(self)
+
+    @classmethod
+    def _check_captures(cls):
+        assert len(cls.targets) > 1, "None active captures"
+        return
+
+    @classmethod
+    def capture(cls):
+        Setup._check_captures()
+
+        cap = cv2.imread(cls.targets[0], cv2.IMREAD_COLOR)
+
+        if cap is not None:
+            h, w = cap.shape[:2]
+            pts = np.array(
+                [[100, 100], [w - 300, 300], [w - 100, h - 100], [100, h - 100]],
+                np.int32,
+            )
+
+            pts = pts.reshape((-1, 1, 2))
+            cv2.imshow("polygons", cap)
+            print(f"Active captures {len(cls.targets)}")
+
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            exit()
+        else:
+            pass
