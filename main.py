1eaa6aca391fb72bf2e76fc9137e40f01e277a50
marhiru <marcosgamern@gmail.com>
2025-12-03 22:16:08 -0300

    feat: testing uniface model

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-from setup import Setup
+import cv2
+from uniface import SCRFD
+from uniface.constants import SCRFDWeights
 
-setup = Setup("./vehicles.png")
+from features.timer import Timer
+
+detector = SCRFD(
+    model_name=SCRFDWeights.SCRFD_10G_KPS,
+    conf_thresh=0.5,
+    nms_thresh=0.4,
+    input_size=(640, 640),
+)
+image = cv2.imread("./tomhanks01.jpeg", cv2.IMREAD_COLOR)
+timer = Timer(True)
+
+if image is not None:
+    faces = detector.detect(image)
+
+    for face in faces:
+        bbox = face["bbox"]
+        confidence = face["confidence"]
+        landmarks = face["landmarks"]
+
+        result = f"bouding boxes: {bbox}\nconfidence: {confidence:.2}\nlandmarks: {landmarks}"
+        print(f"{result}")
+
+        cv2.imshow("Tomhanks", image)
+        timer_debug = timer.debug()
+        cv2.waitKey(0)
+        exit()
 
 
-def main():
-    setup.capture()
+def main() -> int:
+    return 0
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 import cv2
+from cv2.typing import MatLike
 import numpy as np
 
 
 class Setup:
     targets: list[str] = []
 
-    def __init__(self, capture: str):
+    def __init__(self, capture: str) -> None:
         self = capture
         Setup.targets.append(self)
 
     @classmethod
     def _check_captures(cls):
-        assert len(cls.targets) > 1, "None active captures"
+        assert len(cls.targets) >= 1, "None active captures"
         return
 
     @classmethod
-    def capture(cls):
+    def capture(cls) -> MatLike | None:
         Setup._check_captures()
 
         cap = cv2.imread(cls.targets[0], cv2.IMREAD_COLOR)
@@ -33,6 +34,7 @@ class Setup:
 
             cv2.waitKey(0)
             cv2.destroyAllWindows()
+            return cap
             exit()
         else:
             pass
