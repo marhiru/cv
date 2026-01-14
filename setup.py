bc7748dfdd1ba6315f1b8ce6556dac0e59749783
marhiru <marcosgamern@gmail.com>
2026-01-14 00:38:48 -0300

    feat: improved project `setup`

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
+import numpy
 
-setup(ext_modules=cythonize("main.py"))
+extensions = [
+    Extension(
+        name="features.detect",
+        sources=["features/detect.pyx"],
+        include_dirs=[numpy.get_include()],
+        extra_compile_args=["-O3"],
+    )
+]
+
+setup(
+    name="features-detector",
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            "language_level": 3,
+            "boundscheck": False,
+            "wraparound": False,
+            "cdivision": True,
+        },
+    ),
+    zip_safe=False,
+)
