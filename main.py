70441cfe673b5bca440d11082582f4cc6812f782
marhiru <marcosgamern@gmail.com>
2026-01-13 23:11:25 -0300

    fix: testing detection mod

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from features.detection import Detector
 
 
 def main() -> int:
-    Detector()
+    d = Detector(
+        image_path="./tomhanks01.jpeg",
+        show=False,
+    )
+    numbers = [1, 2, 3, 4]
+    d.run()
+
+    doubled_numbers = list(map(lambda n: n * 2, numbers))
+
+    print(doubled_numbers)
     return 0
 
 
-main()
+if __name__ == "__main__":
+    main()
